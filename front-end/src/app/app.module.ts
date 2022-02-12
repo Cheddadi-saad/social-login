e99f2b34da940008888521941d1141dfad173e07
Saad Cheddadi <cheddadi.sc@gmail.com>
2022-02-13 00:38:26 +0100

    angular social login

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SportingComponent } from './sporting/sporting.component';
@@ -10,24 +9,30 @@ import { HeaderComponent } from './componants/header/header.component';
 import { HomeComponent } from './componants/home/home.component';
 import { ProfileComponent } from './componants/profile/profile.component';
 import { SocialComponent } from './componants/social/social.component';
-
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SportingComponent,
-    HeaderComponent,
-    HomeComponent,
-    ProfileComponent,
-    SocialComponent
+  declarations: [AppComponent, SportingComponent, HeaderComponent, HomeComponent, ProfileComponent, SocialComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, SocialLoginModule],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('680577579172-uqqm2l0p3rnsnofr8vj7fna7lb3ptotp.apps.googleusercontent.com'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('347845843869218'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
