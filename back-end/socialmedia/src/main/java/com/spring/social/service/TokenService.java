d66bf20e0841b1ecd81dcbba7851cb13615f1c7b
Saad Cheddadi <cheddadi.sc@gmail.com>
2022-02-14 17:10:15 +0100

    Adding JWT Config and filter

---
+++ back-end/socialmedia/src/main/java/com/spring/social/service/TokenService.java
@@ -0,0 +1,48 @@
+package com.spring.social.service;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.spring.social.dto.JwtLogin;
+import com.spring.social.dto.LoginResponse;
+import com.spring.social.dto.UserPrincipal;
+import com.spring.social.util.JwtProperties;
+
+@Service
+public class TokenService {
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	public TokenService(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+
+	private String generateToken(Authentication authResult) {
+
+		// Grab principal
+		UserPrincipal principal = (UserPrincipal) authResult.getPrincipal();
+		System.out.println(principal.getUsername());
+
+		// Create JWT Token
+		return JWT.create().withSubject(principal.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+				.sign(HMAC512(JwtProperties.SECRET.getBytes()));
+	}
+
+	public LoginResponse login(JwtLogin jwtLogin) throws Exception {
+		Authentication authenticate = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(jwtLogin.getEmail(), jwtLogin.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authenticate);
+		String token = generateToken(authenticate);
+		return new LoginResponse(token);
+	}
+}
